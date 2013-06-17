9d9418ba92d07cd9cb56a69285bc2c9eeda517b0
Daniel Watson <daniel.watsonbros@gmail.com>
2013-06-17 19:25:53 -0400

    Bug fix: Print preview on Linux shows the margin incorrectly.

--- OpenRPT/renderer/orprintrender.cpp
+++ OpenRPT/renderer/orprintrender.cpp
@@ -74,7 +74,8 @@ bool ORPrintRender::setupPrinter(ORODocument * pDocument, QPrinter * pPrinter)
   }
   else 
   {
-    #if defined(Q_WS_MAC) && (QT_VERSION < 0x040801) // QTBUG-20882
+    // On Linux, the print preview has a margin bug, this line fixes it for me using Qt 4.6.4 or Qt 4.8.4.
+    #if (defined(Q_WS_X11) && (QT_VERSION <= 0x040804)) || (defined(Q_WS_MAC) && (QT_VERSION < 0x040801)) // QTBUG-20882
       pPrinter->setPageSize((QPrinter::PageSize)psi.qpValue());
     #else
       pPrinter->setPaperSize(QSizeF(psi.width()/100,psi.height()/100), QPrinter::Inch);
